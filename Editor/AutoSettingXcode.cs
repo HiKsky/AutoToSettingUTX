8b8b3eff02cd76d23a0d09187f2a053ddef557a0
HiKsky <13651251228@163.com>
2017-05-22 14:29:44 +0800

    Auto setting xcode building setting
    Only need setting  *.projmods file,When you see the structure,you wlii know how to use it.

---
+++ Editor/AutoSettingXcode.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+using System.Collections.Generic;
+using System.IO;
+#endif
+
+public class AutoSettingXcode : MonoBehaviour {
+
+	#if UNITY_IOS || UNITY_EDITOR  
+	[PostProcessBuild(999)]  
+	public static void OnPostprocessBuild(BuildTarget BuildTarget, string path)  
+	{  
+		if (BuildTarget == BuildTarget.iOS)  
+		{  
+			string projPath = PBXProject.GetPBXProjectPath(path);  
+			PBXProject proj = new PBXProject();  
+			proj.ReadFromString(File.ReadAllText(projPath));  
+
+			// 获取当前项目名字  
+			string target = proj.TargetGuidByName(PBXProject.GetUnityTargetName());
+			string[] files = Directory.GetFiles (Application.dataPath + "/Editor/", "*.projmods", SearchOption.AllDirectories);
+			foreach (string file in files) {
+				ApplyDataBinding (file, path, proj, target);
+			}
+			// 保存工程  
+			proj.WriteToFile (projPath);  
+		}  
+	}  
+
+	public static void ApplyDataBinding(string pbxmod,string rootpath,PBXProject proj,string target)
+	{
+		DataBinding dataBinding = new DataBinding(pbxmod);
+		ApplyData(dataBinding.libs,(valueOne,valueTwo)=>{
+			proj.AddFileToBuild (target,proj.AddFile("usr/lib/"+valueOne, "Frameworks/"+valueOne, PBXSourceTree.Sdk));
+		});
+		ApplyData(dataBinding.frameworks,(valueOne,valueTwo)=>{
+			proj.AddFrameworkToProject (target, valueOne, false);  
+		});
+		ApplyData(dataBinding.plist,(valueOne,valueTwo)=>{
+			 //修改plist  
+
+			string plistPath = rootpath + "/Info.plist";  
+			 PlistDocument plist = new PlistDocument();  
+			 plist.ReadFromString(File.ReadAllText(plistPath));  
+			 PlistElementDict rootDict = plist.root; 
+			if(valueTwo == "True" || valueTwo == "False")
+			{
+				rootDict.SetBoolean(valueOne,bool.Parse(valueTwo)); 
+				plist.WriteToFile (plistPath);  
+				return;
+			}
+			if(ChargeIsNumber(valueTwo))
+			{
+				rootDict.SetInteger(valueOne,int.Parse(valueTwo));
+				plist.WriteToFile (plistPath);  
+				return;
+			}
+			 // 语音听写需要开启的权限
+			 rootDict.SetString(valueOne, valueTwo);  
+			 //保存plist  
+			 plist.WriteToFile (plistPath); 
+		});
+		Debug.Log("修改PLIST成功!");
+		ApplyData (dataBinding.bitcode, (valueOne, ValueTwo) => {
+			proj.SetBuildProperty (target, "ENABLE_BITCODE", valueOne);  
+		});
+		Debug.Log("修改ENABLE_BITCODE成功!");
+		ApplyData (dataBinding.code_sign_identity,(valueOne,ValueTwo)=>{
+			proj.SetBuildProperty (target, "CODE_SIGN_IDENTITY", valueOne +":"+ ValueTwo);  
+			Debug.Log(valueOne);
+		});
+		Debug.Log("修改CODE_SIGN_IDENTITY成功!");
+		ApplyData (dataBinding.provisioning_profile_specifier,(valueOne,ValueTwo)=>{
+			proj.SetBuildProperty (target, "PROVISIONING_PROFILE_SPECIFIER", valueOne);  
+		});
+		Debug.Log("修改PROVISIONING_PROFILE_SPECIFIER成功!");
+		ApplyData (dataBinding.framework_search_paths,(valueOne,ValueTwo)=>{
+			proj.SetBuildProperty (target, "FRAMEWORK_SEARCH_PATHS", valueOne);  
+		});
+		Debug.Log("修改FRAMEWORK_SEARCH_PATHS成功!");
+		ApplyData (dataBinding.other_ldflags,(valueOne,ValueTwo)=>{
+			proj.SetBuildProperty (target, "OTHER_LDFLAGS", valueOne);  
+		}); 
+		Debug.Log("修改OTHER_LDFLAGS成功!");
+	}
+
+
+	public static void ApplyData(ArrayList data,Action<string,string> callBack)
+	{
+		if(data.Count == 0)return;
+		foreach( string item in data) 
+		{  
+			string[] resources = item.Split (':');
+			if(resources.Length == 1)callBack (resources[0],string.Empty);
+			if(resources.Length == 2)callBack (resources[0],resources[1]);
+		}  
+	}
+
+	public static bool ChargeIsNumber(string message)
+	{
+			System.Text.RegularExpressions.Regex rex=
+			new System.Text.RegularExpressions.Regex(@"^\d+$");
+			if (rex.IsMatch(message))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+	}
+    #endif 
+}
---
+++ Editor/DataBinding.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class DataBinding {
+
+	private Hashtable _datastore = new Hashtable();
+	public string name { get; private set; }
+	public string path { get; private set; }
+
+	public ArrayList libs {
+		get {
+			return (ArrayList)_datastore["libs"];
+		}
+	}
+
+	public ArrayList frameworks {
+		get {
+			return (ArrayList)_datastore ["frameworks"];
+		}
+	}
+
+	public ArrayList headerpaths {
+		get {
+			return (ArrayList)_datastore["headerpaths"];
+		}
+	}
+
+	public ArrayList files {
+		get {
+			return (ArrayList)_datastore["files"];
+		}
+	}
+
+	public ArrayList folders {
+		get {
+			return (ArrayList)_datastore["folders"];
+		}
+	}
+
+	public ArrayList excludes {
+		get {
+			return (ArrayList)_datastore["excludes"];
+		}
+	}
+
+	public ArrayList compiler_flags {
+		get {
+			return (ArrayList)_datastore["compiler_flags"];
+		}
+	}
+
+	public ArrayList linker_flags {
+		get {
+			return (ArrayList)_datastore["linker_flags"];
+		}
+	}
+	public ArrayList plist {
+		get {
+			return (ArrayList)_datastore["plist"];
+		}
+	}
+
+	public ArrayList bitcode {
+		get {
+			return (ArrayList)_datastore["bitcode"];
+		}
+	}
+
+	public ArrayList code_sign_identity {
+		get {
+			return (ArrayList)_datastore["code_sign_identity"];
+		}
+	}
+
+	public ArrayList provisioning_profile_specifier {
+		get {
+			return (ArrayList)_datastore["provisioning_profile_specifier"];
+		}
+	}
+
+	public ArrayList framework_search_paths {
+		get {
+			return (ArrayList)_datastore["framework_search_paths"];
+		}
+	}
+
+	public ArrayList other_ldflags {
+		get {
+			return (ArrayList)_datastore["other_ldflags"];
+		}
+	}
+
+	public DataBinding( string filename )
+	{	
+		FileInfo projectFileInfo = new FileInfo (filename);
+		if (!projectFileInfo.Exists) {
+			Debug.LogWarning ("File does not exist.");
+			return;
+		}
+		name = System.IO.Path.GetFileNameWithoutExtension (filename);
+		path = System.IO.Path.GetDirectoryName (filename);
+		string contents = projectFileInfo.OpenText ().ReadToEnd ();
+		_datastore = (Hashtable)XUPorterJSON.MiniJSON.jsonDecode (contents);
+		if (_datastore == null || _datastore.Count == 0) {
+			throw new UnityException ("Parse error in file " + System.IO.Path.GetFileName (filename) + "! Check for typos such as unbalanced quotation marks, etc.");
+		}
+	}
+}
+	
